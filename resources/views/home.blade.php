6592259ef6b4dc7196c59c783bde3ce9e622d75a
monierjimenez <monierjimenez@gmail.com>
2021-07-01 20:53:26 -0600

    camiospo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('content') hello hola esto es una prueba n
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
